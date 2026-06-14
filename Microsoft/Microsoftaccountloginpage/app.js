d07aa35bf3cad4d10c3f384adba36d75ea4a8ce7
afrin shaik <skafrin7013@gmail.com>
2026-06-14 10:35:10 +0530

    Projects

---
+++ Microsoft/Microsoftaccountloginpage/app.js
@@ -0,0 +1,11 @@
+function next()
+{
+  let email=document.getElementById('email').value
+  if(email=="shaikafrin750@gmail.com")
+  {
+    window.alert("Login Successfully");
+  }
+  else{
+    window.alert("Invalid email");
+  }
+}
---
+++ Microsoft/Microsoftaccountsigninpage/app.js
@@ -0,0 +1,11 @@
+function next()
+{
+  let emailphone=document.getElementById("emailphone").value
+  if(emailphone=="shaikafrin750@gmail.com" || emailphone==7013491025)
+  {
+    window.alert("Sign in Successfully");
+  }
+  else{
+    window.alert("Invalid Details");
+  }
+}
